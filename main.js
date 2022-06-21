7f5b73136c0ccdface62614fcc7c135af6bd4e62
raptor56781 <91291982+raptor56781@users.noreply.github.com>
2022-06-21 12:34:09 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+index.html
+Who has access
+G
+System properties
+Type
+HTML
+Size
+1 KB
+Storage used
+1 KBOwned by WhiteHat Jr
+Location
+ADV-C110-112-Project
+Owner
+Google Workspace
+Modified
+May 3, 2021 by
+Opened
+5:29 PM by me
+Created
+May 3, 2021
+No description
+Viewers can download
+function startClassification()
+{
+  navigator.mediaDevices.getUserMedia({ audio: true, video:false});
+  classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/u02fOYRBx/model.json', { probabilityThreshold: 0.7 } ,modelReady);
+}
+
+function modelReady(){
+  classifier.classify(gotResults);
+}
+
+var dog = 0;
+var cat = 0;
+
+function gotResults(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    random_number_r = Math.floor(Math.random() * 255) + 1;
+    random_number_g = Math.floor(Math.random() * 255) + 1;
+    random_number_b = Math.floor(Math.random() * 255) + 1;
+
+
+    document.getElementById("result_label").innerHTML = 'Detected voice is of  - '+ results[0].label;
+    document.getElementById("result_count").innerHTML = 'Detected Dog - '+dog+ ' Detected Cat - '+cat;
+    document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+    document.getElementById("result_count").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+
+    img = document.getElementById('animal_image');
+
+    if (results[0].label == "Barking") {
+      img.src = 'bark.gif';
+      dog = dog+1;
+    } else if (results[0].label == "Meowing") {
+      img.src = 'meow.gif';
+      cat = cat + 1;
+    } else{
+      img.src = 'listen.gif';
+    }
+  }
+}
